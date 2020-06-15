385757ea8727329fc01f52ce9f0eab3efaa78c81
zheng <1105946494@qq.com>
2020-06-15 20:45:40 +0800

    实现拖动元素

--- test-code/dist/main.1f19ae8e.js
+++ test-code/dist/main.1f19ae8e.js
@@ -118,31 +118,30 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
-// 节流（一段时间执行一次之后，就不执行第二次）
-var a = 1;
+var dragging = false;
+var position = null;
+xxx.addEventListener("mousedown", function (e) {
+  dragging = true;
+  position = [e.clientX, e.clientY];
+});
+document.addEventListener("mousemove", function (e) {
+  if (dragging === false) {
+    return;
+  }
 
-function throttle(fn, delay) {
-  console.log("\u6211\u6267\u884C\u4E86".concat(a, "\u6B21"));
-  a = a + 1;
-  var canUse = true;
-  return function () {
-    console.log("\u5185\u90E8\u6267\u884C\u4E86".concat(a, "\u6B21"));
-
-    if (canUse) {
-      fn.apply(this, arguments);
-      canUse = false;
-      setTimeout(function () {
-        canUse = true;
-      }, delay);
-    }
-  };
-}
-
-var throttled = throttle(function () {
-  return console.log("hi");
-}, 2000);
-throttled();
-throttled();
+  var x = e.clientX;
+  var y = e.clientY;
+  var deltaX = x - position[0];
+  var deltaY = y - position[1];
+  var left = parseInt(xxx.style.left || 0);
+  var top = parseInt(xxx.style.top || 0);
+  xxx.style.left = left + deltaX + "px";
+  xxx.style.top = top + deltaY + "px";
+  position = [x, y];
+});
+document.addEventListener("mouseup", function (e) {
+  dragging = false;
+});
 },{}],"../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -171,7 +170,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52301" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57855" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
